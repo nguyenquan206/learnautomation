03ee5cc8445459b501141cbab0670da5e031534c
nguyenquan <quanbi2005@gmail.com>
2026-03-23 17:29:19 +0700

    Login fail

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,6 +7,7 @@ class LoginPage(BasePage):
     PASSWORD_TEXT = (By.ID, "password")
     LOGGIN_BUTTON = (By.XPATH, "//button[@type='submit']")
     LOGOUT_BUTTON = (By.XPATH, "//a[@href='/logout']")
+    LOGGIN_FAILED_TEXT = (By.XPATH, "//div[@id='flash' and contains(text(),'Your username is invalid!')]")
 
     def input_username(self, username):
         self.send_keys(self.USERNAME_TEXT, username)
@@ -19,3 +20,6 @@ class LoginPage(BasePage):
 
     def is_logout_displayed(self):
         return self.is_element_displayed(self.LOGOUT_BUTTON)
+
+    def is_loggin_fail_text_displayed(self):
+        return self.is_element_displayed(self.LOGGIN_FAILED_TEXT)
